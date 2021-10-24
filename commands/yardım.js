74bef3451e3917163993561a5afc7c4459e4933d
Glitch (snowy-broad-binder) <none>
2021-10-24 05:44:18 +0000

    🚏🍓 Checkpoint ./commands/yardım.js:32033859/94

--- commands/yardım.js
+++ commands/yardım.js
@@ -13,26 +13,26 @@ module.exports = {
         message.channel.send(new Discord.MessageEmbed().setColor('#93ffb5').setAuthor(` ${message.guild.name} Yardım menüsü`)
             .setDescription(`
             > \`.avatar\` -> Avatarınızı gösterir.
-            > \`.ban @üye <sebep>\` -> Üyeyi sunucudan yasaklar.
-            > \`.çek @üye\` -> Üyeyi bulunduğunuz kanala çekersiniz.
-            > \`.git @üye\` -> Üyenin bulunduğu ses kanalına gidersiniz.
-            > \`.jail @üye <sebep>\` -> Üyenin tüm rollerini alarak cezalıya atar.
-            > \`.kick @üye <sebep>\` -> Üyeyi sunucudan atar.
-            > \`.mute @üye <sebep>\` -> Üyeyi metin kanallarında susturur.
-            > \`.rol <al/ver> @üye @rol\` -> Kullanışınıza göre üyeye rol verir veya alır.
-            > \`.rolgöster <id>\` -> Belirttiğiniz rol ile ilgili bilgiler verir.
-            > \`.say\` -> Sunucu bilgilerini verir.
-            > \`.sohbet <aç/kapat>\` -> Kullanış amacınıza göre bulunduğunuz kanalı kapatır veya açar.
-            > \`.tempjail @üye <süre> <sebep>\` -> Üyeyi süreli bir şekilde tüm rollerini alarak jaile atar.
-            > \`.tempmute @üye <süre> <sebep>\` -> Üyeyi metin kanallarında süreli susturur.
-            > \`.tempvmute @üye <süre> <sebep>\` -> Üyeyi ses kanallarında süreli susturur.
-            > \`.unban <id>\` -> Belirttiğiniz idnin yasaklamasını kaldırır.
-            > \`.unjail @üye\` -> Üyeyi cezalıdan çıkararak eski rollerini geri verir.
-            > \`.unmute @üye\` -> Üyenin metin kanallarında ki susturmasını açar.
-            > \`.vmute @üye <sebep>\` -> Üyeyi sesli kanallarda susturur.
-            > \`.vunmute @üye\` -> Üyenin sesli kanallarda ki susturmasını kaldırır.
-            > \`.warn @üye <sebep>\` -> Üyeyi özelden uyarır.
-            > \`.yetkili-say\` -> Aktif olup seste olmayan yetkilileri gösterir.
+            > \`.ban @üye <sebep>\` -> **Üyeyi sunucudan yasaklar.**
+            > \`.çek @üye\` -> **Üyeyi bulunduğunuz kanala çekersiniz.**
+            > \`.git @üye\` -> **Üyenin bulunduğu ses kanalına gidersiniz.**
+            > \`.jail @üye <sebep>\` -> **Üyenin tüm rollerini alarak cezalıya atar.**
+            > \`.kick @üye <sebep>\` -> **Üyeyi sunucudan atar.**
+            > \`.mute @üye <sebep>\` -> **Üyeyi metin kanallarında susturur.**
+            > \`.rol <al/ver> @üye @rol\` -> **Kullanışınıza göre üyeye rol verir veya alır.**
+            > \`.rolgöster <id>\` -> **Belirttiğiniz rol ile ilgili bilgiler verir.**
+            > \`.say\` -> **Sunucu bilgilerini verir.**
+            > \`.sohbet <aç/kapat>\` -> **Kullanış amacınıza göre bulunduğunuz kanalı kapatır veya açar.**
+            > \`.tempjail @üye <süre> <sebep>\` -> **Üyeyi süreli bir şekilde tüm rollerini alarak jaile atar.**
+            > \`.tempmute @üye <süre> <sebep>\` -> **Üyeyi metin kanallarında süreli susturur.**
+            > \`.tempvmute @üye <süre> <sebep>\` -> **Üyeyi ses kanallarında süreli susturur.**
+            > \`.unban <id>\` -> **Belirttiğiniz idnin yasaklamasını kaldırır.**
+            > \`.unjail @üye\` -> **Üyeyi cezalıdan çıkararak eski rollerini geri verir.**
+            > \`.unmute @üye\` -> **Üyenin metin kanallarında ki susturmasını açar.**
+            > \`.vmute @üye <sebep>\` -> **Üyeyi sesli kanallarda susturur.**
+            > \`.vunmute @üye\` -> **Üyenin sesli kanallarda ki susturmasını kaldırır.**
+            > \`.warn @üye <sebep>\` -> **Üyeyi özelden uyarır.**
+            > \`.yetkili-say\` -> **Aktif olup seste olmayan yetkilileri gösterir.**
             
             `))
 
